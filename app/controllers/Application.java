ae39e7e487bab757b2d7fe5a4bb0e53a1e6e02df
Aaron Snyder <a453803@gmail.com>
2014-05-04 14:40:09 -0700

    added a form to refute initial, possibly rude, assumptions

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,29 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index(String adjective, String animal) {
-        return ok(index.render(adjective, animal));
+    public static Result index(String adjective, String noun) {
+        return ok(index.render(form(Correction.class), adjective, noun));
+    }
+
+    public static class Correction {
+        public String adjective;
+        public String noun;
+    }
+
+    public static Result submitCorrection() {
+        Form<Correction> form = form(Correction.class).bindFromRequest();
+        if(form.hasErrors()){
+            return badRequest(index.render(form,"magnificent","donkey"));
+        } else {
+            Correction data = form.get();
+            return ok(index.render(form(Correction.class),data.adjective,data.noun));
+        }
     }
 }
